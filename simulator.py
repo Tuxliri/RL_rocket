16fa6682b2171fde935aa8047712ac44ab350e61
Davide <diafrate@elecnor.es>
2022-05-12 11:29:34 +0200

    Implemented functions to switch between coordinate frames _localToGlobal and _globalToLocal

--- simulator.py
+++ simulator.py
@@ -5,10 +5,11 @@ from math import radians
 import numpy as np
 from scipy import integrate
 
+
 class Simulator():
-    def __init__(self, IC, dt = 0.01, dynamics = 'std3DOF') -> None:
-        super(Simulator,self).__init__()
-        
+    def __init__(self, IC, dt=0.01, dynamics='std3DOF') -> None:
+        super(Simulator, self).__init__()
+
         self.dynamics = dynamics
         self.timestep = dt
         self.t = 0
@@ -32,12 +33,13 @@ class Simulator():
         self.x_CP = 20                      # Center of pressure [m]
         self.Sref = 50                      # Reference surface [m^2]
         self.x_PVP = 0                      # Thrust gimbal point [m]
+
         pass
 
     def step(self):
 
         if self.dynamics == 'std3DOF':
-            return integrate.solve_ivp(self.RHS, [self.t, 60+ self.t + self.dt], self.state).y[:,-1]
+            return integrate.solve_ivp(self.RHS, [self.t, 60 + self.t + self.dt], self.state).y[:, -1]
 
         elif self.dynamics == 'linear3DOF':
             raise NotImplementedError()
@@ -51,60 +53,99 @@ class Simulator():
             raise NotImplementedError()
 
     def RHS(self, t, y):
-            # extract dynamics variables
-            x, z, th, dx, dz, dth = y
+        # extract dynamics variables
+        x, z, th, dx, dz, dth = y
 
-            # Get control variables
-            T = 0#*u[0]
-            beta = 0#*u[1]
+        # Get control variables
+        T = 0  # *u[0]
+        beta = 0  # *u[1]
 
-            # Implement getting it from the height (z)
-            rho = 1.225
+        # Implement getting it from the height (z)
+        rho = 1.225
 
-            alfa = 0
-            # alfa = np.atan2(...)
-            # Compute aerodynamic coefficients
-            Cn = self.Cnalfa*alfa
-            Cd = self.Cdalfa*alfa
+        alfa = 0
+        alfa = self._computeAoA(y)
 
-            # Compute aero forces
-            v2 = dx**2 + dz**2
-            Q = 0.5*rho*v2
+        # Compute aerodynamic coefficients
+        Cn = self.Cnalfa*alfa
+        Cd = self.Cdalfa*alfa
 
-            D = Cd*Q*self.Sref
+        # Compute aero forces
+        v2 = dx**2 + dz**2
+        Q = 0.5*rho*v2
 
-            N = Cn*Q*self.Sref
+        D = Cd*Q*self.Sref
 
-            g = self.g0
-            
-            # Torque arms
-            l_alfa = self.x_CP - self.x_CG
-            l_c = self.x_CG - self.x_PVP
+        N = Cn*Q*self.Sref
 
-            # Compute state derivatives
-            ddx = (T*np.cos(beta) - D)/self.m
-            ddz = g*np.cos(th) + dx*dth + (-N - T*np.sin(beta))/self.m
-            ddth = (l_alfa*N - l_c*T*np.sin(beta))/self.I
-            # dm = T/(self.Isp*self.g0)
-            
-            dy = np.array([dx, dz, dth, ddx, ddz, ddth])
+        g = self.g0
 
-            return dy
+        # Torque arms
+        l_alfa = self.x_CP - self.x_CG
+        l_c = self.x_CG - self.x_PVP
+
+        # Compute state derivatives
+        ddx = (T*np.cos(beta) - D)/self.m
+        ddz = g*np.cos(th) + dx*dth + (-N - T*np.sin(beta))/self.m
+        ddth = (l_alfa*N - l_c*T*np.sin(beta))/self.I
+        # dm = T/(self.Isp*self.g0)
+
+        dy = np.array([dx, dz, dth, ddx, ddz, ddth])
+
+        return dy
 
     def _computeAoA(self, state):
         if self.dynamics == 'std3DOF':
             vx = state[3]
             vz = state[4]
 
-            alfa = np.arctan2(vz,vx)
+            alfa = np.arctan2(vz, vx)
 
         elif self.dynamics == '6DOF':
             raise NotImplementedError
 
         else:
-             raise NotImplementedError
+            raise NotImplementedError
 
         return alfa
+
+    def _localToGlobal(self, stateLocal):
+        if self.dynamics == 'std3DOF':
+            theta = stateLocal[2]
+            ROT = np.array([np.cos(theta), np.sin(theta)],
+                           [-np.sin(theta), np.cos(theta)])
+            stateGlobal = stateLocal
+            stateGlobal[0:2] = np.dot(
+                ROT, stateLocal[0:2]
+            )
+            stateGlobal[3:5] = np.dot(
+                ROT, stateLocal[3:5]
+            )
+        elif self.dynamics == '6DOF':
+            raise NotImplementedError
+
+        else:
+            raise NotImplementedError
+
+        return stateGlobal
+
+    def _globalToLocal(self, stateGlobal):
+        if self.dynamics == 'std3DOF':
+            theta = stateGlobal[2]
+            ROT = np.array([[np.cos(theta), -np.sin(theta)],
+                           [np.sin(theta), np.cos(theta)]])
+            stateLocal = stateGlobal
+            stateLocal[0:2] = ROT @ stateGlobal[0:2]
+            stateLocal[3:5] = ROT @ stateGlobal[3:5]
+
+        elif self.dynamics == '6DOF':
+            raise NotImplementedError
+
+        else:
+            raise NotImplementedError
+
+        return stateLocal
+
 if __name__ == "__main__":
     IC = np.array([10, 10, 10, 3, 4, 5])
     RKT1 = Simulator(IC)
