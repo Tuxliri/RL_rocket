9c8260e17ae8fc4d9a2adc46beee2a0311cf0833
Davide <diafrate@elecnor.es>
2022-05-27 17:13:24 +0200

    implemented model save and load

--- rocket_env.py
+++ rocket_env.py
@@ -300,8 +300,12 @@ if __name__ == "__main__":
     print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
     
     # Train the agent
-    model.learn(total_timesteps=500000)
+    model.learn(total_timesteps=100000)
+    # Save the agent
+    model.save("PPO_goddard")
+    del model  # delete trained model to demonstrate loading
 
+    model = PPO.load("PPO_goddard")
     # Evaluate the trained agent
     mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
 
