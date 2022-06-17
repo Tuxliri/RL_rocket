46a0f895bcfe8d03def08f86033e609caf6be75e
Davide <diafrate@elecnor.es>
2022-06-17 17:11:45 +0200

    Created 3DOF discrete action wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -14,3 +14,21 @@ class DiscreteActions(gym.ActionWrapper):
     
     def action(self, act):
         return self.disc_to_cont[act]
+
+class DiscreteActions3DOF(gym.ActionWrapper):
+    def __init__(
+        self,
+        env,
+        disc_to_cont = [[-1, -1], [-1, 0], [-1, +1],
+                        [0, -1], [0, 0], [0, +1],
+                        [+1, -1], [+1, 0], [+1, +1]]
+        ):
+        super().__init__(env)
+        # Create an action table for all possible 
+        # combinations of the values of thrust and
+        # gimbaling
+        self.disc_to_cont = disc_to_cont
+        self._action_space = Discrete(len(disc_to_cont))
+    
+    def action(self, act):   
+        return np.asarray(self.disc_to_cont[act])
