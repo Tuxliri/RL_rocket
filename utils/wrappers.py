ad908a25bc915e6c8df4eea10d8ff1c1fb96caa5
Davide <diafrate@elecnor.es>
2022-06-21 17:22:54 +0200

    Added default keymappings to use with gym.play()

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -15,6 +15,11 @@ class DiscreteActions(gym.ActionWrapper):
     def action(self, act):
         return self.disc_to_cont[act]
 
+    def get_keys_to_action(self):
+        import pygame
+        mapping = {(pygame.K_UP,): 1, (pygame.K_DOWN,): 0}
+        return mapping
+
 class DiscreteActions3DOF(gym.ActionWrapper):
     def __init__(
         self,
@@ -26,9 +31,16 @@ class DiscreteActions3DOF(gym.ActionWrapper):
         super().__init__(env)
         # Create an action table for all possible 
         # combinations of the values of thrust and
-        # gimbaling
+        # gimbaling action = [delta, thrust]
+        
         self.disc_to_cont = disc_to_cont
         self._action_space = Discrete(len(disc_to_cont))
     
     def action(self, act):   
         return np.asarray(self.disc_to_cont[act])
+
+    def get_keys_to_action(self):
+        import pygame
+        mapping = {(pygame.K_LEFT,): 8, (pygame.K_RIGHT,): 2,
+            (pygame.K_UP,): 5, (pygame.K_MODE,): 3}
+        return mapping
