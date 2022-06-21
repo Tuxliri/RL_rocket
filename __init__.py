85496c24b2b46e640f546f735e6a87a8e846c15c
Davide <diafrate@elecnor.es>
2022-06-21 17:20:14 +0200

    Removed timestep limit on environment registration

--- __init__.py
+++ __init__.py
@@ -4,11 +4,9 @@ from gym.envs.registration import register
 register(
     id='Falcon-v0',
     entry_point='my_environment.main:make1Drocket',
-    max_episode_steps=4000
 )
 
 register(
     id='Falcon3DOF-v0',
-    entry_point='my_environment.main:makerocket',
-    max_episode_steps=4000
+    entry_point='my_environment.main:makerocket'
 )
