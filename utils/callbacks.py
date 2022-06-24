fb05ecc4ed533b9d85a737967a14e5944b835d78
Davide <diafrate@elecnor.es>
2022-06-24 16:06:32 +0200

    Record states on final timestep of episode

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -7,20 +7,21 @@ class FigureRecorderCallback(BaseCallback):
         super(FigureRecorderCallback, self).__init__(verbose)
 
     def _on_step(self) -> bool:
+        if self.locals['dones'][0] is True:
+            showFig = False
+            # [0] needed as the method returns a list containing the tuple of figures
+            states_fig, action_fig = self.training_env.env_method("plotStates", showFig)[
+                0]
+
+            # Close the figure after logging it
+
+            self.logger.record("States", Figure(states_fig, close=True),
+                            exclude=("stdout", "log", "json", "csv"))
+            self.logger.record("Thrust", Figure(action_fig, close=True),
+                            exclude=("stdout", "log", "json", "csv"))
+
         return super()._on_step()
 
     def _on_rollout_end(self) -> None:
-        showFig = False
-        # [0] needed as the method returns a list containing the tuple of figures
-        states_fig, action_fig = self.training_env.env_method("plotStates", showFig)[
-            0]
-
-        # Close the figure after logging it
-
-        self.logger.record("States", Figure(states_fig, close=True),
-                           exclude=("stdout", "log", "json", "csv"))
-        self.logger.record("Thrust", Figure(action_fig, close=True),
-                           exclude=("stdout", "log", "json", "csv"))
-
+        
         return super()._on_rollout_end()
-
