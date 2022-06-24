bd79628d9ba3d31b04b3b06376717c18b4729926
Davide <diafrate@elecnor.es>
2022-06-24 16:07:05 +0200

    Registering environment as module

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
-setup(name='my_environment',
+setup(name='rocket_environment',
 version='0.0.1',
-install_requires=['gym==0.21.0', 'scipy==1.7.3'])
+install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame'])
