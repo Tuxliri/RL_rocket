ee804e769b1f5ffb2227156d08fb24d814dce074
Tuxliri <Tuxliri@users.noreply.github.com>
2022-06-29 22:17:37 +0200

    Update rocket_env.py

--- my_environment/envs/rocket_env.py
+++ my_environment/envs/rocket_env.py
@@ -137,7 +137,7 @@ class Rocket(Env):
         # Penalize the rocket going upward
         vy = self.y[4]
         upward_vel_rew=0
-        if vy > 5.:
+        if vy > 6.:
             upward_vel_rew = - np.log(vy - 5.)
 
         reward = dist_reward + pose_reward + upward_vel_rew + rotation_rew
