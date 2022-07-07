32e84a7910d78b2932381ba08fa7d003d538372b
Davide <diafrate@elecnor.es>
2022-07-07 16:32:26 +0200

    Added numpy package to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='my_environment',
     version='0.0.1',
-    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2'],
+    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.19.1'],
     include_package_data=True,
     package_dir={"": "my_environment"}
     )
