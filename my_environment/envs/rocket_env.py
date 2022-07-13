Fixed target velocity computation

my_environment/envs/rocket_env.py
@@ -127,8 +127,8 @@ class Rocket(Env):
         self.infos.append(info)
 
         if done and not currentTime>=self.maxTime:
-            assert self._checkCrash(obs) or self._checkLanding(obs), f"self._checkCrash is {self._checkCrash} and self._checkLanding is f{self._checkLanding}"
-
+            # assert self._checkCrash(obs) or self._checkLanding(obs), f"self._checkCrash is {self._checkCrash} and self._checkLanding is f{self._checkLanding}"
+            pass
         return self._normalize_obs(obs), reward, done, info
 
     def _normalize_obs(self, obs):
@@ -484,7 +484,7 @@ class Rocket(Env):
 
         y = state[1]
         __, vy = state[3:5]
-        glideslope = np.arctan2(vy/v)
+        glideslope = np.arctan2(vy,v)
 
         v_lim = 2
         r_lim = 5