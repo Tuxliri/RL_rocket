779e685aca5475fe78eb877b823ee1dbfe5421d1
Tuxliri <dvde.iafrate98@gmail.com>
2022-07-22 14:40:48 +0200

    Applied black styling

--- my_environment/wrappers/wrappers.py
+++ my_environment/wrappers/wrappers.py
@@ -1,4 +1,9 @@
-__all__ = ['DiscreteActions', 'DiscreteActions3DOF', 'GaudetStateObs','RecordVideoFigure']
+__all__ = [
+    "DiscreteActions",
+    "DiscreteActions3DOF",
+    "GaudetStateObs",
+    "RecordVideoFigure",
+]
 
 import os
 from typing import Callable
@@ -13,49 +18,47 @@ from gym import logger
 import wandb
 import pandas as pd
 
+
 class DiscreteActions(gym.ActionWrapper):
-    def __init__(
-        self,
-        env,
-        disc_to_cont= [np.array([-1]), np.array([1])]
-        ):
+    def __init__(self, env, disc_to_cont=[np.array([-1]), np.array([1])]):
         super().__init__(env)
         self.disc_to_cont = disc_to_cont
         self._action_space = Discrete(len(disc_to_cont))
-    
+
     def action(self, act):
         return self.disc_to_cont[act]
 
     def get_keys_to_action(self):
         import pygame
+
         mapping = {(pygame.K_UP,): 1, (pygame.K_DOWN,): 0}
         return mapping
 
+
 class DiscreteActions3DOF(gym.ActionWrapper):
-    def __init__(
-        self,
-        env,
-        disc_to_cont = [
-        [0, -1], [-1, +1],
-        [0, +1], [+1, +1]
-                    ]
-    ):
+    def __init__(self, env, disc_to_cont=[[0, -1], [-1, +1], [0, +1], [+1, +1]]):
         super().__init__(env)
-        # Create an action table for all possible 
+        # Create an action table for all possible
         # combinations of the values of thrust and
         # gimbaling action = [delta, thrust]
-        
+
         self.disc_to_cont = disc_to_cont
         self._action_space = Discrete(len(disc_to_cont))
-    
-    def action(self, act):   
+
+    def action(self, act):
         return np.asarray(self.disc_to_cont[act])
 
     def get_keys_to_action(self):
         import pygame
-        mapping = {(pygame.K_LEFT,): 1, (pygame.K_RIGHT,): 3,
-            (pygame.K_UP,): 2, (pygame.K_MODE,): 0}
+
+        mapping = {
+            (pygame.K_LEFT,): 1,
+            (pygame.K_RIGHT,): 3,
+            (pygame.K_UP,): 2,
+            (pygame.K_MODE,): 0,
+        }
         return mapping
+
 
 class GaudetStateObs(gym.ObservationWrapper):
     def __init__(self, env: Rocket) -> None:
@@ -63,23 +66,33 @@ class GaudetStateObs(gym.ObservationWrapper):
         self.observation_space = Box(low=-1, high=1, shape=(4,))
 
     def observation(self, observation):
-        x,y,th = observation[0:3]
-        vx,vy,vth = observation[3:6]
+        x, y, th = observation[0:3]
+        vx, vy, vth = observation[3:6]
 
-        r=np.array([x,y])
-        v=np.array([vx,vy])
-        
-        v_targ, t_go = self.env.unwrapped.compute_vtarg(r,v)
+        r = np.array([x, y])
+        v = np.array([vx, vy])
+
+        v_targ, t_go = self.env.unwrapped.compute_vtarg(r, v)
         vx_targ, vy_targ = v_targ
-        
-        return np.float32([vx-vx_targ, vy-vy_targ, t_go,y])
+
+        return np.float32([vx - vx_targ, vy - vy_targ, t_go, y])
+
 
 class RecordVideoFigure(RecordVideo):
-    def __init__(self, env, video_folder: str, image_folder: str, episode_trigger: Callable[[int], bool] = None,
-                step_trigger: Callable[[int], bool] = None, video_length: int = 0, 
-                name_prefix: str = "rl-video"):
-        super().__init__(env, video_folder, episode_trigger, step_trigger, video_length, name_prefix)
-        
+    def __init__(
+        self,
+        env,
+        video_folder: str,
+        image_folder: str,
+        episode_trigger: Callable[[int], bool] = None,
+        step_trigger: Callable[[int], bool] = None,
+        video_length: int = 0,
+        name_prefix: str = "rl-video",
+    ):
+        super().__init__(
+            env, video_folder, episode_trigger, step_trigger, video_length, name_prefix
+        )
+
         self.image_folder = os.path.abspath(image_folder)
         # Create output folder if needed
         if os.path.isdir(self.image_folder):
@@ -97,36 +110,40 @@ class RecordVideoFigure(RecordVideo):
         if self.episode_trigger(self.episode_id):
             if not self.is_vector_env:
                 if dones:
-                    fig_states,fig_actions = self.env.unwrapped.plot_states()
+                    fig_states, fig_actions = self.env.unwrapped.plot_states()
                     plt.close()
                     plt.close()
 
                     fig_rew = pd.DataFrame(self.rewards_info).plot()
 
                     if wandb.run is not None:
-                        wandb.log({
-                            "states": fig_states,
-                            "actions" : fig_actions,
-                            "rewards" : fig_rew,
-                            "landing_success" : float(infos["perfect_landing"])
-                            })
+                        wandb.log(
+                            {
+                                "states": fig_states,
+                                "actions": fig_actions,
+                                "rewards": fig_rew,
+                                "landing_success": float(infos["perfect_landing"]),
+                            }
+                        )
                     else:
                         self.save_figure(fig_states, "states_figure")
                         self.save_figure(fig_actions, "actions_figure")
-                    
+
             elif dones[0]:
-                fig_states,fig_actions = self.env.unwrapped.plot_states()
+                fig_states, fig_actions = self.env.unwrapped.plot_states()
                 plt.close()
                 plt.close()
 
                 fig_rew = pd.DataFrame(self.rewards_info).plot()
 
                 if wandb.run is not None:
-                    wandb.log({
-                        "states": fig_states,
-                        "actions" : fig_actions,
-                        "rewards" : fig_rew,
-                        })
+                    wandb.log(
+                        {
+                            "states": fig_states,
+                            "actions": fig_actions,
+                            "rewards": fig_rew,
+                        }
+                    )
                 else:
                     self.save_figure(fig_states, "states_figure")
                     self.save_figure(fig_actions, "actions_figure")
@@ -138,12 +155,12 @@ class RecordVideoFigure(RecordVideo):
     def reset(self, **kwargs):
         self.rewards_info = []
         return super().reset(**kwargs)
-        
+
     def save_figure(self, figure: plt.Figure, prefix):
         figure_name = f"{prefix}-step-{self.step_id}"
         if self.episode_trigger:
             figure_name = f"{prefix}-step-{self.episode_id}"
-        
+
         base_path = os.path.join(self.image_folder, figure_name)
 
         figure.savefig(base_path)
