365dd694e1c31a73c0774329e1c9a1f2953a0ad6
Tuxliri <dvde.iafrate98@gmail.com>
2022-07-29 16:30:18 +0200

    Removed unused 1D env and wrappers
    *Deleted the code wrapping the 3D environment to 1D
    
    *Deleted the wrapper for the discrete actions of the 1D env

--- my_environment/wrappers/wrappers.py
+++ my_environment/wrappers/wrappers.py
@@ -1,5 +1,4 @@
 __all__ = [
-    "DiscreteActions",
     "DiscreteActions3DOF",
     "GaudetStateObs",
     "RewardAnnealing",
@@ -18,23 +17,6 @@ import numpy as np
 from gym import logger
 import wandb
 import pandas as pd
-
-
-class DiscreteActions(gym.ActionWrapper):
-    def __init__(self, env, disc_to_cont=[np.array([-1]), np.array([1])]):
-        super().__init__(env)
-        self.disc_to_cont = disc_to_cont
-        self._action_space = Discrete(len(disc_to_cont))
-
-    def action(self, act):
-        return self.disc_to_cont[act]
-
-    def get_keys_to_action(self):
-        import pygame
-
-        mapping = {(pygame.K_UP,): 1, (pygame.K_DOWN,): 0}
-        return mapping
-
 
 class DiscreteActions3DOF(gym.ActionWrapper):
     def __init__(self, env, disc_to_cont=[[0, -1], [-1, +1], [0, +1], [+1, +1]]):
