c1eae1d88a47e565d98fc1c3142f69eba7e3630f
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-18 16:19:24 +0200

    Added 6DOF environment registration

--- my_environment/__init__.py
+++ my_environment/__init__.py
@@ -5,3 +5,8 @@ register(
     id='my_environment/Falcon3DOF-v0',
     entry_point='my_environment.envs:Rocket'
 )
+
+register(
+    id='my_environment/Falcon6DOF-v0',
+    entry_point='my_environment.envs:Rocket6DOF'
+)
