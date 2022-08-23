cf7531995876736f3358819e64152f332a437506
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-23 17:24:31 +0200

    Modified initial conditions and rewards

--- configuration_file.py
+++ configuration_file.py
@@ -1,3 +1,5 @@
+from numpy import pi
+
 config = {
     "env_id" : "my_environment/Falcon3DOF-v0",
     "policy_type": "MlpPolicy",
@@ -6,15 +8,15 @@ config = {
     "max_time" : 150,
     "RANDOM_SEED" : 42,
     "INITIAL_CONDITIONS" : [500, 100, 100,
-                            0, -10, -10,
+                            0, 0, 0,
                             1, 0, 0, 0,
-                            0,0,0,
-                            50e3],
-    "IC_RANGE" : [50,10,10,
-                1,1,1,
+                            0,0,0.5,
+                            45e3],
+    "IC_RANGE" : [0,0,0,
+                0,0,0,
                 0,0,0,0,
                 0,0,0,
-                1e3],
+                0],
     "reward_coefficients" : {
                             "alfa" : -0.01, 
                             "beta" : 0,
@@ -28,7 +30,10 @@ config = {
             "waypoint" : 50,
             "landing_radius" : 30,
             "maximum_velocity" : 10,
-            "attitude_lim" : [1,0,0,0], #TODO: COMPUTE THE CORRECT VALUES!!
+            "attitude_lim" : [1.5, 1.5, 2*pi], # [Yaw, Pitch, Roll],
+                                                # rotations order zyx
+                                                # VISUALIZATION:
+                                                # https://bit.ly/3CoEdvH 
             "omega_lim" : [0.2, 0.2, 0.2]
         }
 }
