9bdfa916dc6b9410e737e5bf590d8a7b267cbe7f
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-23 17:25:31 +0200

    Testing environment compliance with sb3 and gym

--- test_6DOF_sb_integration.py
+++ test_6DOF_sb_integration.py
@@ -5,15 +5,21 @@ from my_environment.envs import Rocket6DOF
 from stable_baselines3.ppo import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from gym.wrappers import RecordVideo
-
+from stable_baselines3.common.env_checker import check_env
+    
 # Import the initial conditions from the setup file
 from configuration_file import config
 
 # Instantiate the environment
 env = Rocket6DOF(IC=config["INITIAL_CONDITIONS"])
+# Check for the environment compatibility with gym and sb3
+#check_env(env, skip_render_check=False)
+
+del env
+env = Rocket6DOF(IC=config["INITIAL_CONDITIONS"])
 
 # Test usage with stable_baselines_3 model
-model = PPO('MlpPolicy', env, verbose=0)
+model = PPO('MlpPolicy', env, verbose=1)
 
 # Use a separate environement for evaluation
 eval_env = RecordVideo(Rocket6DOF(IC=config["INITIAL_CONDITIONS"]),'6DOF_videos',video_length=500)
@@ -23,7 +29,7 @@ import time
 start_time = time.time()
 
 # Random Agent, before training
-mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=5,render=True)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5,render=True)
 finish_time = time.time()
 
 print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
