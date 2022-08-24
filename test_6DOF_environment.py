d7bf398b10bcd36163f6957be36cde45fb338d94
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-24 18:03:16 +0200

    Merged ppo-6DOF-new-rendering into ppo-6DOF

--- test_6DOF_environment.py
+++ test_6DOF_environment.py
@@ -1,7 +1,9 @@
 """
 Script to test functionality of the 6DOF environment
 """
+
 from my_environment.envs import Rocket6DOF
+from gym.wrappers import RecordVideo
 
 # Import the initial conditions from the setup file
 from configuration_file import config
@@ -15,19 +17,19 @@ env = Rocket6DOF(
 # env=RecordVideo(env,video_folder="video_6DOF")
 
 # [delta_y, delta_z, thrust]
-null_action = [0.,0.,-1]
-non_null_action = [1.,1.,-0.5]
+null_action = [0.0, 0.0, -1]
+non_null_action = [1.0, 1.0, -0.5]
 
 # Initialize the environment
 done = False
 obs = env.reset()
-env.render(mode='human')
+env.render(mode="human")
 
 while not done:
-    obs,rew,done,info = env.step(null_action)
-    env.render(mode='human')
+    obs, rew, done, info = env.step(null_action)
+    env.render(mode="human")
 
-fig=env.get_trajectory_plotly()
+fig = env.get_attitude_trajectory()
 env.close()
 
 fig.show()
