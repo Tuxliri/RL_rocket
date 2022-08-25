36f27e09ce1ea3a885bb65a6263b50269025f699
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-25 12:34:44 +0200

    Moved to dictionary to env instantiation
    * Changed the names of the dictionary keys to allow passing as **kwargs

--- configuration_file.py
+++ configuration_file.py
@@ -1,43 +1,45 @@
+from distutils.command.config import config
 from numpy import pi
 
 env_config = {
-    "timestep" : 0.05,
-    "seed" : 42,
-    "IC" : [500, 100, 100,
-                            0, 0, 0,
-                            1, 0, 0, 0,
-                            0,0,0.5,
-                            45e3],
-    "ICRange" : [0,0,0,
-                0,0,0,
-                0,0,0,0,
-                0,0,0,
-                0],
-    "reward_coeff" : {
-                            "alfa" : -0.01, 
-                            "beta" : 0,
-                            "delta" : -5,
-                            "eta" : 0.2,
-                            "gamma" : -10,
-                            "kappa" : 10,
-                            "xi" : 0.004,
-                            },
-    "trajectory_limits": {"attitude_limit": [1.5, 1.5, 2*pi]},
-    "landing_params" : {
-            "waypoint" : 50,
-            "landing_radius" : 30,
-            "maximum_velocity" : 10,
-            "landing_attitude_limit" : [10/180*pi, 10/180*pi, 2*pi], # [Yaw, Pitch, Roll] in RAD,
-                                                # rotations order zyx
-                                                # VISUALIZATION:
-                                                # https://bit.ly/3CoEdvH 
-            "omega_lim" : [0.2, 0.2, 0.2]
-        }
+    "timestep": 0.05,
+    "seed": 42,
+    "IC": [500, 100, 100, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0.5, 45e3],
+    "ICRange": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "reward_coeff": {
+        "alfa": -0.01,
+        "beta": 0,
+        "delta": -5,
+        "eta": 0.2,
+        "gamma": -10,
+        "kappa": 10,
+        "xi": 0.004,
+    },
+    "trajectory_limits": {"attitude_limit": [1.5, 1.5, 2 * pi]},
+    "landing_params": {
+        "waypoint": 50,
+        "landing_radius": 30,
+        "maximum_velocity": 10,
+        "landing_attitude_limit": [
+            10 / 180 * pi,
+            10 / 180 * pi,
+            2 * pi,
+        ],  # [Yaw, Pitch, Roll] in RAD,
+        # rotations order zyx
+        # VISUALIZATION:
+        # https://bit.ly/3CoEdvH
+        "omega_lim": [0.2, 0.2, 0.2],
+    },
 }
 
+TOTAL_TIMESTEP = int(1e6)
+MAX_TIME = 150
+
 sb3_config = {
-    "env_id" : "my_environment/Falcon3DOF-v0",
+    "env_id": "my_environment/Falcon6DOF-v0",
     "policy_type": "MlpPolicy",
-    "total_timesteps": int(5e3),
-    "max_time" : 150,
+    "total_timesteps": TOTAL_TIMESTEP,
+    "max_time": MAX_TIME,
+    "max_ep_timesteps": MAX_TIME / env_config["timestep"],
+    "eval_freq": TOTAL_TIMESTEP/20,
 }
