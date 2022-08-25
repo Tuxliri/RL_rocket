41964289918f3c152aa5fb1bb4c05d173e743e20
Tuxliri <dvde.iafrate98@gmail.com>
2022-08-25 13:17:36 +0200

    Added pyvista requirement

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='my_environment',
     version='0.0.2',
-    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.21.*', 'wandb'],
+    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.21.*', 'wandb','pyvista'],
     include_package_data=True,
     package_dir={"": "my_environment"}
     )
