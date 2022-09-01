783b70fc02562b7adb9de8c5844b830350e836ea
Tuxliri <dvde.iafrate98@gmail.com>
2022-09-01 16:35:11 +0200

    Refactored make_eval_env function

--- main_6DOF.py
+++ main_6DOF.py
@@ -45,7 +45,9 @@ def start_training():
         )
 
     def make_eval_env():
-        training_env = make_env()
+        kwargs = env_config
+        training_env = gym.make("my_environment/Falcon6DOF-v0",**kwargs)
+        training_env = TimeLimit(training_env, max_episode_steps=sb3_config["max_ep_timesteps"])
         return EpisodeAnalyzer(training_env)
         # return EpisodeAnalyzer6DOF(training_env,video_folder=f"videos_6DOF/{run.id}",
             # episode_trigger=lambda x: x%5==0)
