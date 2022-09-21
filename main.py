a7af0609ee3c12b4c181c49d574bb68270739f7d
Tuxliri <Tuxliri@users.noreply.github.com>
2022-09-21 15:15:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         tensorboard_log=f"runs/{run.id}",
         verbose=1,
         seed=config["RANDOM_SEED"],
-        ent_coef=0.01,
+        ent_coef=0.001,
         )
 
     def make_eval_env():
