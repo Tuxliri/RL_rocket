25bffb8dadd6afd2a0a2e126c4f5b77ad5985454
Tuxliri <dvde.iafrate98@gmail.com>
2022-10-04 10:54:41 +0200

    Config moved outside of main

--- main.py
+++ main.py
@@ -19,30 +19,7 @@ import my_environment
 from my_environment.wrappers.wrappers import *
 from gym.wrappers import TimeLimit, RecordVideo
 
-config = {
-    "env_id" : "my_environment/Falcon3DOF-v0",
-    "policy_type": "MlpPolicy",
-    "total_timesteps": int(6e6),
-    "timestep" : 0.1,
-    "max_time" : 100,
-    "RANDOM_SEED" : 42,
-    "initial_conditions" : [-1600, 2000, np.pi*3/4, 180, -90, 0, 41e3],
-    "initial_conditions_range" : [200,10,0.1,30,30,0.05,1e3],
-    "reward_coefficients" : {
-                            "alfa" : -0.01, 
-                            "beta" : -1e-8,
-                            "delta" : -5,
-                            "eta" : 0.1,
-                            "gamma" : -10,
-                            "kappa" : 10,
-                            "xi" : 0.004,
-                            "landing_radius" : 50,
-                            "w_r_f" : 0.1,
-                            "w_v_f" : 0.8,
-                            "max_r_f": 100,
-                            "max_v_f": 50,
-                            },
-}
+from config import config
 
 config["max_ep_timesteps"] = int(config["max_time"]/config["timestep"])
 config["eval_freq"] = int(config["total_timesteps"]/20)
